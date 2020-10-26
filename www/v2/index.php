64b83a6c355aa17a8f3da6d6627e018e19dac5e7
dataweb <bert@data-web.be>
2020-10-26 19:57:30 +0100

    vendor

--- www/v2/index.php
+++ www/v2/index.php
@@ -2,9 +2,9 @@
 error_reporting(E_ALL & ~E_NOTICE);
 
 
-require __DIR__."/../vendor/autoload.php";
+require __DIR__."/../../vendor/autoload.php";
 require 'autoload.php';
-
+ 
 use dwApi\dwApi;
 use dwApi\output\OutputFactory;
 
