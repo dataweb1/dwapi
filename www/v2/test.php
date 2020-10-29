f81bd2c08ae2295676134f7eef2bd1fcd15053f7
dataweb <bert@data-web.be>
2020-10-29 14:39:06 +0100

    no message

--- www/v2/test.php
+++ www/v2/test.php
@@ -1,24 +1,29 @@
 <?php
-$curl = curl_init();
+try{
+  $curl = curl_init();
 
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://api.data-web.be/v1/user/login?project=LluG3gwZKPzC",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "email=bert%40data-web.be&password=test",
-));
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => "http://dwapi.local/v2/item/read?project=&entity=",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+  ));
 
-$response = curl_exec($curl);
-$err = curl_error($curl);
+  $response = curl_exec($curl);
+  $err = curl_error($curl);
 
-curl_close($curl);
+  curl_close($curl);
 
-if ($err) {
-  echo "cURL Error #:" . $err;
-} else {
-  echo $response;
+  if ($err) {
+    echo "cURL Error #:" . $err;
+  } else {
+    echo $response;
+  }
+
+} catch (Throwable $exception) { //Use Throwable to catch both errors and exceptions
+  header('HTTP/1.1 500 Internal Server Error'); //Tell the browser this is a 500 error
+  echo $e->getMessage();
 }
