89a506122d54c0e64b7aa961542389f629551fab
dataweb <bert@data-web.be>
2021-02-04 20:31:23 +0100

    clean up

--- www/_v2/autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-function custom_autoloader($class) {
-  if (strpos($class, "dwApi\\") !== false) {
-    include __DIR__ . "/" . str_replace("\\", "/", $class) . '.php';
-  }
-}
-
-spl_autoload_register('custom_autoloader');
--- www/index.php
+++ www/index.php
@@ -8,8 +8,10 @@ require __DIR__."/../vendor/autoload.php";
 use dwApi\dwApi;
 use dwApi\output\OutputFactory;
 
+$api_path = "http://localhost/";
+$reference_path = "https://dataweb.stoplight.io/api/v1/projects/dataweb/dwapi/nodes/reference/dwapi.json";
 
-$api = new dwApi();
+$api = new dwApi($api_path, $reference_path);
 $api->processCall();
 
 $output = OutputFactory::create();
