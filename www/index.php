8bba30161262ca23eb01dfb14b9831ebd410330c
dataweb <bert@data-web.be>
2021-02-19 12:51:16 +0100

    no message

--- www/index.php
+++ www/index.php
@@ -1,6 +1,7 @@
 <?php
 error_reporting(E_ALL & ~E_NOTICE);
 
+echo $_SERVER["DOCUMENT_ROOT"];
 
 require __DIR__."/../vendor/autoload.php";
 require __DIR__.'/dwapi/autoload.php';
@@ -9,10 +10,10 @@ use dwApi\dwApi;
 use dwApiLib\output\OutputFactory;
 
 $settings = new stdClass();
-$settings->project = "staging_AbcDefGhiJkl";//"CFNIOuwTJGyR";
+//$settings->project = "staging_AbcDefGhiJkl";//"CFNIOuwTJGyR";
 $settings->api_path = "http://localhost/";
-//$settings->reference_path = "https://dataweb.stoplight.io/api/v1/projects/dataweb/dwapi/nodes/reference/dwapi.json";
-$settings->reference_path = "https://api.eeed.eu/reference/eeed_api.v1.yaml";
+$settings->reference_path = "https://dataweb.stoplight.io/api/v1/projects/dataweb/dwapi/nodes/reference/dwapi.json";
+//$settings->reference_path = "https://api.eeed.eu/reference/eeed_api.v1.yaml";
 $settings->template_path = $_SERVER["DOCUMENT_ROOT"]."/templates";
 
 $api = new dwApi($settings);
