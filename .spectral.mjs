e585d51115f4ac9032c54853ef35c9cf789396eb
bert@data-web.be <bert@data-web.be>
2023-10-27 12:11:15 +0200

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjEyMTEz/spectral.js?branch_slug=main&token=9f0f5715-4b62-4797-a615-8fa52adb5b8a";
+export default { extends: ruleset };
